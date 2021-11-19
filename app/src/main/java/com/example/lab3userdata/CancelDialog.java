39a6eb7e7eb51e412b2f1c5789cc9491c28a4d4c
Dasha Sarnatskaya <dasha.sarnatskaya@gmail.com>
2021-11-19 21:32:20 +0300

    update cancelDialog

--- app/src/main/java/com/example/lab3userdata/CancelDialog.java
+++ app/src/main/java/com/example/lab3userdata/CancelDialog.java
@@ -1,9 +1,11 @@
 package com.example.lab3userdata;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
@@ -25,5 +27,16 @@ public class CancelDialog extends DialogFragment {
                 });
 
         return builder.create();
+    }
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        clickListener = (ClickListener) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        clickListener = null;
     }
 }
